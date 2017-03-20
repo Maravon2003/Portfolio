4ab1f5a4774e736b46fffc3bccffef86880a0c5e
Tamara <maravon2003@gmail.com>
2017-03-20 23:59:00 +0000

    something

--- script.js
+++ script.js
@@ -11,3 +11,14 @@ $(document).ready(function(){
 $("#submit-name").click(function() {
 $("p").append( $("#name").val() );
 });
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $("#div1").fadeIn();
+        $("#div2").fadeIn("slow");
+        $("#div3").fadeIn(3000);
+    });
+});
+$(selector).animate({params},speed,callback);
+
+
